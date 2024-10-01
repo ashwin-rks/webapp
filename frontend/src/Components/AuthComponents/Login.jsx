e4bbf55a238f7e3c7a7e357d7c7ed222e280a85b
Ashwin R <ashwin.r@jmangroup.com>
2024-10-01 10:49:22 +0530

    Connected login and signup

--- frontend/src/Components/AuthComponents/Login.jsx
+++ frontend/src/Components/AuthComponents/Login.jsx
@@ -1,21 +1,90 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import "../styles.css";
 import "./authStyles.css";
 
-
 const Login = () => {
   const navigate = useNavigate();
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
 
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
+
+  // set visibility for password
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
 
-return (
+  // stores changes in field to state
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let newErrors = {};
+
+    // Checks for errors in Email field
+    if (!credentials.email.trim()) {
+      newErrors = {
+        ...newErrors,
+        email: "Email is required!",
+      };
+    } else if (!emailRegex.test(credentials.email.trim())) {
+      newErrors = {
+        ...newErrors,
+        email: "Enter a valid email!",
+      };
+    }
+
+    // Checks for errors in Password field
+    if (!credentials.password.trim()) {
+      newErrors = {
+        ...newErrors,
+        password: "Password is required!",
+      };
+    }
+
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+      const email = credentials.email;
+      const password = credentials.password;
+      try {
+        const response = await axios.post("http://localhost:8000/auth/login", {
+          email,
+          password,
+        });
+        if (response.status === 200) {
+          localStorage.setItem("token", response.data.token);
+          navigate("/home");
+        } else {
+          console.error("Login failed:", response.data.error);
+        }
+      } catch (error) {
+        console.error("Error during login:", error);
+        newErrors = {
+          ...newErrors,
+          password: "Email or Password is invalid!",
+        };
+        setErrors(newErrors);
+      }
+    }
+  };
+
+  return (
     <div className="container-fluid vh-100 login-background">
       {/* Top Center Title */}
       <div
@@ -30,9 +99,9 @@ return (
         <div className="col-md-8 d-none d-md-flex justify-content-center align-items-center">
           <div className="text-left">
             <h1 className="font-weight-bold motivationText fontColor">
-              Improvise.
+              Im<span className="highlight-text">pro</span>vise.
               <br />
-              <span style={{ color: "#06bee1" }}>Adapt.</span>
+              <span className="sub-text">Adapt.</span>
               <br />
               Overcome.
             </h1>
@@ -43,11 +112,11 @@ return (
         <div className="col-md-4 d-flex justify-content-center align-items-center">
           <div className="login-form">
             <form
-              className="roundedBorder d-flex flex-column justify-content-center gap-3 p-4 fontColor"
+              className="roundedBorder d-flex flex-column justify-content-center p-4 fontColor"
               style={{ backgroundColor: "white" }}
             >
               <h3 className="text-center fs-1 fw-bolder">Login</h3>
-              <div className="form-group">
+              <div className="form-group mt-3">
                 <label htmlFor="email" className="fs-5">
                   Email
                 </label>
@@ -57,9 +126,15 @@ return (
                   id="email"
                   name="email"
                   autoComplete="true"
+                  onChange={handleChange}
                 />
               </div>
-              <div className="form-group position-relative">
+              {errors.email && (
+                <div className="mt-1 p-0">
+                  <p className="small text-danger m-0 p-0">{errors.email}</p>
+                </div>
+              )}
+              <div className="form-group position-relative mt-3">
                 <label htmlFor="password" className="fs-5">
                   Password
                 </label>
@@ -69,6 +144,7 @@ return (
                   id="password"
                   name="password"
                   autoComplete="true"
+                  onChange={handleChange}
                 />
                 <span
                   className="position-absolute"
@@ -77,7 +153,7 @@ return (
                     right: "10px",
                     cursor: "pointer",
                     transform: "translateY(-50%)",
-                    height: "1em", 
+                    height: "1em",
                     display: "flex",
                     alignItems: "center",
                   }}
@@ -88,13 +164,19 @@ return (
                   />
                 </span>
               </div>
+              {errors.password && (
+                <div className="mt-1 p-0">
+                  <p className="small text-danger p-0">{errors.password}</p>
+                </div>
+              )}
               <button
                 type="submit"
-                className="btn btn-primary btnColor fs-5 p-2"
+                className="btn btn-primary btnColor fs-5 p-2 mt-3"
+                onClick={handleSubmit}
               >
                 Login
               </button>
-              <div className="d-flex justify-content-center">
+              <div className="d-flex justify-content-center mt-2">
                 <p className="m-0 p-0">Not an user?</p>
                 <button className="notBtn" onClick={() => navigate("/signup")}>
                   Signup
@@ -106,6 +188,6 @@ return (
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
--- frontend/src/Components/AuthComponents/Signup.jsx
+++ frontend/src/Components/AuthComponents/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
@@ -12,6 +13,106 @@ const Signup = () => {
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
+  };
+
+  const [credentials, setCredentials] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    account_type: "",
+  });
+  const [errors, setErrors] = useState({});
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Handles the login form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let newErrors = {};
+
+    // Checks for errors in Email field
+    if (!credentials.email.trim()) {
+      newErrors = {
+        ...newErrors,
+        email: "Email is required!",
+      };
+    } else if (!emailRegex.test(credentials.email.trim())) {
+      newErrors = {
+        ...newErrors,
+        email: "Enter a valid email!",
+      };
+    }
+
+    // Checks for errors in Password field
+    if (!credentials.password.trim()) {
+      newErrors = {
+        ...newErrors,
+        password: "Password is required!",
+      };
+    }
+
+    if (!credentials.account_type.trim()) {
+      newErrors = {
+        ...newErrors,
+        account_type: "Account type is required!",
+      };
+    }
+
+    if (!credentials.first_name.trim()) {
+      newErrors = {
+        ...newErrors,
+        first_name: "First Name is required!",
+      };
+    }
+
+    if (!credentials.last_name.trim()) {
+      newErrors = {
+        ...newErrors,
+        last_name: "Last Name is required!",
+      };
+    }
+
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+      const first_name = credentials.first_name;
+      const last_name = credentials.last_name;
+      const account_type = credentials.account_type;
+      const email = credentials.email;
+      const password = credentials.password;
+
+      try {
+        const response = await axios.post("http://localhost:8000/auth/signup", {
+          first_name,
+          last_name,
+          account_type,
+          email,
+          password,
+        });
+        console.log(response);
+        if (response.status === 201) {
+          console.log("Signup successful", response.data);
+          navigate("/login");
+        } else {
+          console.error("Signup failed:", response.data.message);
+        }
+      } catch (error) {
+        console.error("Error during Signup:", error);
+        newErrors = {
+          ...newErrors,
+          password: "Email or Password is invalid!",
+        };
+        setErrors(newErrors);
+      }
+    }
   };
 
   return (
@@ -29,9 +130,9 @@ const Signup = () => {
         <div className="col-md-8 d-none d-md-flex justify-content-center align-items-center">
           <div className="text-left">
             <h1 className="font-weight-bold motivationText fontColor">
-              Improvise.
+              Im<span className="highlight-text">pro</span>vise.
               <br />
-              <span style={{ color: "#06bee1" }}>Adapt.</span>
+              <span className="sub-text">Adapt.</span>
               <br />
               Overcome.
             </h1>
@@ -42,11 +143,11 @@ const Signup = () => {
         <div className="col-md-4 d-flex justify-content-center align-items-center">
           <div className="signup-form">
             <form
-              className="roundedBorder d-flex flex-column justify-content-center gap-3 p-4 fontColor"
+              className="roundedBorder d-flex flex-column justify-content-center p-4 fontColor"
               style={{ backgroundColor: "white" }}
             >
               <h3 className="text-center fs-1 fw-bolder">Sign Up</h3>
-              <div className="form-group">
+              <div className="form-group mt-3">
                 <label htmlFor="email" className="fs-5">
                   Email
                 </label>
@@ -56,9 +157,15 @@ const Signup = () => {
                   id="email"
                   name="email"
                   autoComplete="true"
+                  onChange={handleChange}
                 />
               </div>
-              <div className="form-group">
+              {errors.email && (
+                <div className="mt-1 p-0">
+                  <p className="small text-danger m-0 p-0">{errors.email}</p>
+                </div>
+              )}
+              <div className="form-group mt-3">
                 <label htmlFor="first_name" className="fs-5">
                   First Name
                 </label>
@@ -68,9 +175,17 @@ const Signup = () => {
                   id="first_name"
                   name="first_name"
                   autoComplete="true"
+                  onChange={handleChange}
                 />
               </div>
-              <div className="form-group">
+              {errors.first_name && (
+                <div className="mt-1 p-0">
+                  <p className="small text-danger m-0 p-0">
+                    {errors.first_name}
+                  </p>
+                </div>
+              )}
+              <div className="form-group mt-3">
                 <label htmlFor="last_name" className="fs-5">
                   Last Name
                 </label>
@@ -80,9 +195,17 @@ const Signup = () => {
                   id="last_name"
                   name="last_name"
                   autoComplete="true"
+                  onChange={handleChange}
                 />
               </div>
-              <div className="form-group">
+              {errors.last_name && (
+                <div className="mt-1 p-0">
+                  <p className="small text-danger m-0 p-0">
+                    {errors.last_name}
+                  </p>
+                </div>
+              )}
+              <div className="form-group mt-3">
                 <label htmlFor="account_type" className="fs-5">
                   User Role
                 </label>
@@ -90,12 +213,24 @@ const Signup = () => {
                   className="form-control fs-5"
                   id="account_type"
                   name="account_type"
+                  onChange={handleChange}
+                  value={credentials.account_type}
                 >
+                  <option value="" disabled>
+                    Select account type
+                  </option>
                   <option value="user">User</option>
                   <option value="admin">Admin</option>
                 </select>
               </div>
-              <div className="form-group position-relative">
+              {errors.account_type && (
+                <div className="mt-1 p-0">
+                  <p className="small text-danger m-0 p-0">
+                    {errors.account_type}
+                  </p>
+                </div>
+              )}
+              <div className="form-group position-relative mt-3">
                 <label htmlFor="password" className="fs-5">
                   Password
                 </label>
@@ -105,6 +240,7 @@ const Signup = () => {
                   id="password"
                   name="password"
                   autoComplete="true"
+                  onChange={handleChange}
                 />
                 <span
                   className="position-absolute"
@@ -113,7 +249,7 @@ const Signup = () => {
                     right: "10px",
                     cursor: "pointer",
                     transform: "translateY(-50%)",
-                    height: "1em", 
+                    height: "1em",
                     display: "flex",
                     alignItems: "center",
                   }}
@@ -124,13 +260,19 @@ const Signup = () => {
                   />
                 </span>
               </div>
+              {errors.password && (
+                <div className="mt-1 p-0">
+                  <p className="small text-danger p-0">{errors.password}</p>
+                </div>
+              )}
               <button
                 type="submit"
-                className="btn btn-primary btnColor fs-5 p-2"
+                className="btn btn-primary btnColor fs-5 p-2 mt-3"
+                onClick={handleSubmit}
               >
                 Sign Up
               </button>
-              <div className="d-flex justify-content-center">
+              <div className="d-flex justify-content-center mt-2">
                 <p className="m-0 p-0">Already an User?</p>
                 <button className="notBtn" onClick={() => navigate("/login")}>
                   Login
